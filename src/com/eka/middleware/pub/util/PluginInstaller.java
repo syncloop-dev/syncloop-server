e696e163af660ba2517a1f0c8867d6cf98807cd2
Ekamw <info@ekamw.org>
2023-07-18 23:17:34 +0530

    Refactored.

--- src/com/eka/middleware/pub/util/PluginInstaller.java
+++ src/com/eka/middleware/pub/util/PluginInstaller.java
@@ -6,11 +6,14 @@ import com.eka.middleware.service.PropertyManager;
 import com.eka.middleware.template.SnippetException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import com.nimbusds.jose.shaded.gson.Gson;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.Optional;
@@ -62,13 +65,41 @@ public class PluginInstaller {
 
             // Update in plugin-package.json
 
+            updatePackagePlugin(plugin, dataPipeline);
+
         } else {
             dataPipeline.put("status", false);
         }
     }
 
-    public static Boolean compareDigest(String filePath, String url) throws Exception {
+    private static Boolean compareDigest(String filePath, String url) throws Exception {
         String fileDigest = calculateFileChecksum(filePath);
         return StringUtils.equals(url, fileDigest);
+    }
+
+    /**
+     * @param plugins
+     * @param dataPipeline
+     * @throws IOException
+     */
+    private static void updatePackagePlugin(Plugins plugins, DataPipeline dataPipeline) throws IOException {
+        PluginPackage pluginPackage = new PluginPackage();
+        File file = new File(PropertyManager.getPackagePath(dataPipeline.rp.getTenant()) + "builds/plugin-package.json");
+        if (!file.exists()) {
+            pluginPackage = new PluginPackage();
+            pluginPackage.setPlugins(Lists.newArrayList(plugins));
+        } else {
+            pluginPackage = new Gson().fromJson(new FileReader(file), PluginPackage.class);
+            Optional<Plugins> pluginObj = pluginPackage.getPlugins().parallelStream().filter(f -> f.getUnique_id().equals(plugins.getUnique_id())).findAny();
+
+            if (pluginObj.isPresent()) {
+                Plugins oldPlugin = pluginObj.get();
+                pluginPackage.getPlugins().remove(oldPlugin);
+                pluginPackage.getPlugins().add(plugins);
+            } else {
+                pluginPackage.setPlugins(Lists.newArrayList(plugins));
+            }
+        }
+        IOUtils.write(new Gson().toJson(pluginPackage), new FileOutputStream(file), StandardCharsets.UTF_8);
     }
 }
