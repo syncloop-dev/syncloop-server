197335f2d675222b06c0395f4fe785bb147661ab
GuptaAdarsh10 <123458574+GuptaAdarsh10@users.noreply.github.com>
2023-07-19 20:48:31 +0530

    add checkForUpdate and arePluginsInstalled

--- src/com/eka/middleware/pub/util/PluginInstaller.java
+++ src/com/eka/middleware/pub/util/PluginInstaller.java
@@ -3,9 +3,6 @@ package com.eka.middleware.pub.util;
 import com.eka.middleware.server.Build;
 import com.eka.middleware.service.DataPipeline;
 import com.eka.middleware.service.PropertyManager;
-import com.eka.middleware.template.SnippetException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import com.nimbusds.jose.shaded.gson.Gson;
 import org.apache.commons.io.IOUtils;
@@ -16,6 +13,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 import java.util.Optional;
 
 import static com.eka.middleware.pub.util.AutoUpdate.*;
@@ -24,10 +22,9 @@ public class PluginInstaller {
 
     public static void downloadFile(String pluginId, String version, DataPipeline dataPipeline) throws Exception {
 
-        String jsonUrl = "syncloop-marketplace.json";
+        MarketPlace marketPlace = getMarketPlace();
+        //List<Plugins> plugins = marketPlace.getPlugins();
 
-        String json = readJsonFromUrl(Build.DISTRIBUTION_REPO + jsonUrl);
-        MarketPlace marketPlace = new Gson().fromJson(json, MarketPlace.class);
         Optional<Plugins> pluginObj = marketPlace.getPlugins().parallelStream().filter(f -> f.getUnique_id().equals(pluginId)).findAny();
 
         if (!pluginObj.isPresent()) {
@@ -38,6 +35,11 @@ public class PluginInstaller {
 
         int existed_latest_version_number = plugin.getLatest_version_number();
         // Read version number from plugin-package.json
+
+        int versionNumber = getVersionNumberFromFile(dataPipeline);
+        if (existed_latest_version_number <= versionNumber) {
+            return;
+        }
 
         String fileName = String.format("%sv%s.zip", plugin.getName_slug(), version);
         String filePath = PropertyManager.getPackagePath(dataPipeline.rp.getTenant()) + "builds/import/" + fileName;
@@ -71,7 +73,14 @@ public class PluginInstaller {
             dataPipeline.put("status", false);
         }
     }
+    private static MarketPlace getMarketPlace() throws IOException {
+        String jsonUrl = "syncloop-marketplace.json";
 
+        String json = readJsonFromUrl(Build.DISTRIBUTION_REPO + jsonUrl);
+
+        return new Gson().fromJson(json, MarketPlace.class);
+
+    }
     private static Boolean compareDigest(String filePath, String url) throws Exception {
         String fileDigest = calculateFileChecksum(filePath);
         return StringUtils.equals(url, fileDigest);
@@ -101,5 +110,52 @@ public class PluginInstaller {
             }
         }
         IOUtils.write(new Gson().toJson(pluginPackage), new FileOutputStream(file), StandardCharsets.UTF_8);
+    }
+
+    private static int getVersionNumberFromFile(DataPipeline dataPipeline) throws IOException {
+
+        File file = new File(PropertyManager.getPackagePath(dataPipeline.rp.getTenant()) + "builds/plugin-package.json");
+
+        if (file.exists()) {
+            PluginPackage pluginPackage = new Gson().fromJson(new FileReader(file), PluginPackage.class);
+            List<Plugins> plugins = pluginPackage.getPlugins();
+
+            if (!plugins.isEmpty()) {
+                return plugins.get(0).getLatest_version_number();
+            }
+        }
+
+        return 0;
+    }
+
+    public static Boolean checkForUpdate( List<Plugins> plugins,DataPipeline dataPipeline) throws Exception {
+        int versionNumberFromFile = getVersionNumberFromFile(dataPipeline);
+
+        for (Plugins plugin : plugins) {
+            if (plugin.getLatest_version_number() > versionNumberFromFile) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean arePluginsInstalled(List<Plugins> plugins, DataPipeline dataPipeline) throws IOException {
+
+        String filePath = PropertyManager.getPackagePath(dataPipeline.rp.getTenant()) + "builds/plugin-package.json";
+        File file = new File(filePath);
+
+        if (file.exists()) {
+
+            PluginPackage pluginPackage = new Gson().fromJson(new FileReader(file), PluginPackage.class);
+
+            List<Plugins> installedPlugins = pluginPackage.getPlugins();
+
+            for (Plugins plugin : plugins) {
+                if (installedPlugins.stream().anyMatch(p -> p.getUnique_id().equals(plugin.getUnique_id()))) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
