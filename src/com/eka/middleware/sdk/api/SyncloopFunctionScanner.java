58828c0d498cfa43a2c023cd7b96dfd57e95545f
Ekamw <info@ekamw.org>
2024-05-23 20:07:34 +0530

    Supported * JavaFunctions in Service

--- src/com/eka/middleware/sdk/api/SyncloopFunctionScanner.java
+++ src/com/eka/middleware/sdk/api/SyncloopFunctionScanner.java
@@ -2,13 +2,29 @@ package com.eka.middleware.sdk.api;
 
 import com.beust.jcommander.internal.Lists;
 import com.eka.middleware.sdk.api.outline.*;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.nimbusds.jose.shaded.gson.Gson;
+import com.sun.jdi.event.EventSet;
 import test.Test;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.Parameter;
+import javax.management.AttributeList;
+import javax.management.openmbean.TabularDataSupport;
+import javax.management.relation.RoleList;
+import javax.management.relation.RoleUnresolvedList;
+import javax.print.attribute.standard.PrinterStateReasons;
+import javax.script.Bindings;
+import javax.script.SimpleBindings;
+import javax.swing.*;
+import java.awt.*;
+import java.beans.beancontext.BeanContext;
+import java.lang.reflect.*;
+import java.security.AuthProvider;
+import java.security.Provider;
+import java.util.*;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.jar.Attributes;
 
 public class SyncloopFunctionScanner {
 
@@ -70,7 +86,8 @@ public class SyncloopFunctionScanner {
             for (int i = 0 ; i < parameters.length ; i++) {
                 IOOutline ioOutline = new IOOutline();
                 ioOutline.setText(parametersName[i]);
-                ioOutline.setType(parameters[i].getType().getSimpleName().toLowerCase());
+                //ioOutline.setType(parameters[i].getType().getSimpleName().toLowerCase());
+                ioOutline.setType(mapTypeToString(parameters[i].getParameterizedType(), true));
                 input.add(ioOutline);
             }
 
@@ -84,7 +101,7 @@ public class SyncloopFunctionScanner {
             List<IOOutline> output = Lists.newArrayList();
             IOOutline ioOutline = new IOOutline();
             ioOutline.setText(outputParameterName);
-            ioOutline.setType(returnType.getSimpleName().toLowerCase());
+            ioOutline.setType(mapTypeToString(method.getGenericReturnType(), true));
             output.add(ioOutline);
 
             IOOutline out = new IOOutline();
@@ -103,4 +120,67 @@ public class SyncloopFunctionScanner {
         return serviceOutlines;
     }
 
+
+    private static String mapTypeToString(Type type, boolean isSimpleType) {
+        String dataType = "object";
+        if (type == String.class && isSimpleType) {
+            dataType = "string";
+        } else if (type == Integer.class && isSimpleType) {
+            dataType = "integer";
+        } else if ((type == Double.class || type == Float.class || type == Long.class || type == Number.class) && isSimpleType) {
+            dataType = "number";
+        } else if (type == Date.class && isSimpleType) {
+            dataType = "date";
+        } else if (type == Byte.class && isSimpleType) {
+            dataType = "byte";
+        } else if (type == Boolean.class && isSimpleType) {
+            dataType = "boolean";
+        } else if (MAP_CLASSES.contains(type) && isSimpleType) {
+            dataType = "document";
+        } else if (type instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+            Type rawType = parameterizedType.getRawType();
+            Type actualTypeArgument = parameterizedType.getActualTypeArguments()[0];
+            if (COLLECTION_CLASSES.contains(rawType)) {
+                dataType = mapTypeToString(actualTypeArgument, true) + "List";
+            } else if (MAP_CLASSES.contains(rawType)) {
+                dataType = mapTypeToString(rawType, true);
+            }
+        }
+        if (dataType.contains("ListList")) {
+            dataType = dataType.replaceAll("List", "") + "List";
+        }
+        return dataType;
+    }
+
+
+    private static final Set<Class> MAP_CLASSES = Sets.newHashSet(
+            AbstractMap.class,
+            Attributes.class, AuthProvider.class,
+            ConcurrentHashMap.class,
+            ConcurrentSkipListMap.class, Map.class,
+            EnumMap.class,
+            HashMap.class,
+            com.eka.middleware.heap.HashMap.class,
+            Hashtable.class, IdentityHashMap.class, LinkedHashMap.class,
+            PrinterStateReasons.class, Properties.class, Provider.class,
+            RenderingHints.class, SimpleBindings.class, TabularDataSupport.class, TreeMap.class, UIDefaults.class, WeakHashMap.class,
+            Bindings.class, ConcurrentMap.class, ConcurrentNavigableMap.class, NavigableMap.class, SortedMap.class
+    );
+
+    private static final Set<Class> COLLECTION_CLASSES = Sets.newHashSet(
+            BlockingDeque.class, BlockingQueue.class, Deque.class,
+            EventSet.class, List.class, NavigableSet.class, Queue.class, Set.class,
+            SortedSet.class, TransferQueue.class,
+            AbstractCollection.class,
+            AbstractList.class, AbstractQueue.class, AbstractSequentialList.class, AbstractSet.class,
+            ArrayBlockingQueue.class, ArrayDeque.class, ArrayList.class, AttributeList.class,
+            ConcurrentHashMap.KeySetView.class,
+            ConcurrentLinkedDeque.class, ConcurrentLinkedQueue.class, ConcurrentSkipListSet.class, CopyOnWriteArrayList.class, CopyOnWriteArraySet.class,
+            DelayQueue.class, EnumSet.class, HashSet.class, LinkedBlockingDeque.class,
+            LinkedBlockingQueue.class, LinkedHashSet.class, LinkedList.class, LinkedTransferQueue.class,
+            PriorityBlockingQueue.class, PriorityQueue.class, RoleList.class,
+            RoleUnresolvedList.class,
+            Stack.class, SynchronousQueue.class, TreeSet.class, Vector.class
+    );
 }
